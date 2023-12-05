59ae62a1aa3bb000455f9bc0a8afec029bb209d3
noname1112392 <149623681+noname1112392@users.noreply.github.com>
2023-12-05 11:40:12 -0800

    FInished problem 4

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ alert( isEmpty(schedule) ); // false
 */
 
 let schedule = {
- ton : 'ton',
+    ton: 'ton',
 };
 
 function isEmpty(obj) {
@@ -36,3 +36,74 @@ function isEmpty(obj) {
     }
     return true;
 }
+
+//Third problem ------------------------
+/*We have an object storing salaries of our team:
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
+
+If salaries is empty, then the result must be 0.
+*/
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130,
+}
+
+let sum = 0;
+
+
+function salary() {
+    if (isEmpty(salaries) === false) {
+        for (let salary in salaries) {
+            sum += salaries[salary];
+        }
+        console.log(sum);
+    } else {
+        console.log('0');
+    }
+}
+
+//Fourth problem --------------------------
+/*Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
+
+For instance:
+
+// before the call
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+multiplyNumeric(menu);
+
+// after the call
+menu = {
+  width: 400,
+  height: 600,
+  title: "My menu"
+};
+Please note that multiplyNumeric does not need to return anything. It should modify the object in-place.
+
+P.S. Use typeof to check for a number here.
+*/
+
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+
+function multiplyNumeric(obj) {
+    for (let key in obj) {
+      if (typeof obj[key] == 'number') {
+        obj[key] *= 2;
+      }
+    }
+  }
