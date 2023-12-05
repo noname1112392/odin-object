2ca3579eecb81dc5f76aea9a871eb28401ca3e1c
noname1112392 <149623681+noname1112392@users.noreply.github.com>
2023-12-05 11:17:57 -0800

    Finished problem 2

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //First problem -----------------------------
 /*Write the code, one line for each action:
-
 Create an empty object user.
 Add the property name with the value John.
 Add the property surname with the value Smith.
@@ -12,3 +11,28 @@ user.name = 'John';
 user.surname = 'Smith';
 user.name = 'Pete';
 delete user.name;
+
+//Second problem -----------------
+/*Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
+
+Should work like that:
+
+let schedule = {};
+
+alert( isEmpty(schedule) ); // true
+
+schedule["8:30"] = "get up";
+
+alert( isEmpty(schedule) ); // false
+*/
+
+let schedule = {
+ ton : 'ton',
+};
+
+function isEmpty(obj) {
+    for (let key in obj) {
+        return false;
+    }
+    return true;
+}
